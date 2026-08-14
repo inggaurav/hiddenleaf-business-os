a3bf464c381f6565a536cd4c9bac8bd31a3ded70
gaurav sharma <123371047+inggaurav@users.noreply.github.com>
2026-08-14 17:13:21 +0530

    feat(account): register granular Account permission middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Console\Commands\InstallBusinessOs;
 use App\Console\Commands\ReconcileFinancialBalancesCommand;
 use App\Console\Commands\ValidateParityEvidenceCommand;
 use App\Http\Middleware\CheckModuleStatus;
+use App\Http\Middleware\EnsureAccountPermission;
 use App\Http\Middleware\EnsureApiWorkspace;
 use App\Http\Middleware\EnsureFinancialIdempotency;
 use App\Http\Middleware\EnsureTenantContext;
@@ -42,6 +43,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'module.status' => CheckModuleStatus::class,
             'api.workspace' => EnsureApiWorkspace::class,
+            'account.permission' => EnsureAccountPermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
